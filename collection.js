f83d9dfcebbfc8ac315d48aa11147a992c2df1b3
Franquinet Aubry <aubry.franquinet@hotmail.com>
2021-09-14 18:56:46 +0200

    second commit

---
+++ collection.js
@@ -0,0 +1,90 @@
+const COLLECTION = [{
+        name: 'TEAM VITALITY',
+        logo: 'link/to/a/logo',
+        country: 'France',
+        T1trophies: 5,
+        coach: 'Rémy "XTQZZZ" Quoniam',
+        players: ['Dan "apEX" Madesclaire', 'Mathieu "ZywOo" Herbaut', 'Richard "shox" Papillon', 'Kévin "misutaaa" Rabier', 'Jayson "Kyojin" Nguyen Van']
+    },
+
+    {
+        name: 'NATUS VINCERE',
+        logo: 'link/to/a/logo',
+        country: 'Ukraine',
+        T1trophies: 9,
+        coach: 'Andrii "B1ad3" Gorodenskyi',
+        players: ['Oleksandr "S1mple" Kostyliev', 'Denis "electronic" Sharipov', 'Kirill "Boombl4" Mikhailov', 'Ilya "Perfecto" Zalutskiy', 'Valerii "B1T" Vakhovskyi']
+    },
+
+    {
+        name: 'GAMBIT',
+        logo: 'link/to/a/logo',
+        country: 'Russia',
+        T1trophies: 5,
+        coach: 'Ivan "F_1N" Kochugov',
+        players: ['Vladislav "nafany" Gorshkov', 'Dmitry "sh1ro" Sokolov', 'Timofey "interz" Yakushin', 'Sergey "Ax1Le" Rykhtorov', 'JAbay "Hobbit" Khassenov']
+    },
+
+    {
+        name: 'G2',
+        logo: 'link/to/a/logo',
+        country: 'Europe',
+        T1trophies: 4,
+        coach: 'Damien "maLeK" Marcel',
+        players: ['Audric "JaCkz" Jug', 'François "AmaNEk" Delaunay', 'Nemanja "nexa" Isaković', 'Nemanja "huNter-" Kovač', 'Nikola "NiKo" Kovač']
+    },
+
+    {
+        name: 'HEROIC',
+        logo: 'link/to/a/logo',
+        country: 'Danemark',
+        T1trophies: 5,
+        coach: '',
+        players: ['', '', '', '', '']
+    },
+
+    {
+        name: 'FAZE',
+        logo: 'link/to/a/logo',
+        country: 'Europe',
+        T1trophies: 5,
+        coach: '',
+        players: ['', '', '', '', '']
+    },
+
+    {
+        name: 'VIRTUS.PRO',
+        logo: 'link/to/a/logo',
+        country: 'Russia',
+        T1trophies: 5,
+        coach: '',
+        players: ['', '', '', '', '']
+    },
+
+    {
+        name: 'ASTRALIS',
+        logo: 'link/to/a/logo',
+        country: 'Danemark',
+        T1trophies: 5,
+        coach: '',
+        players: ['', '', '', '', '']
+    },
+
+    {
+        name: 'NIP',
+        logo: 'link/to/a/logo',
+        country: 'Sweden',
+        T1trophies: 5,
+        coach: '',
+        players: ['', '', '', '', '']
+    },
+
+    {
+        name: 'OG',
+        logo: 'link/to/a/logo',
+        country: 'Europe',
+        T1trophies: 5,
+        coach: '',
+        players: ['', '', '', '', '']
+    }
+]
